a8967f20600eaa69119318f2ff39e11b6d384be4
Supreeth <supreeth.k.murthy@gmail.com>
2023-11-07 00:35:34 +0530

    Commits until lecture 106

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,4 +1,4 @@
-import {mount} from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -12,13 +12,14 @@ export default () => {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
-        if (pathname !== nextPathname) history.push(nextPathname);
-      }
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
     });
 
     history.listen(onParentNavigate);
-
   }, []);
 
-  return <div ref={ref} />
-}
+  return <div ref={ref} />;
+};
